8dc3bfd85906b06898de52f993d9e6c09ac71d8d
170042 <170042@E0412-05.JC-21.local>
2020-06-09 12:47:00 +0900

    kadai10

--- src/main/java/jp/te4a/spring/boot/myapp10/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp10/BookRepository.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp9;
+package jp.te4a.spring.boot.myapp10;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<BookBean, Integer>{
