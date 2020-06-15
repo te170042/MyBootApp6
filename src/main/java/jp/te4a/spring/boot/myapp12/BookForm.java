4941a7488f08381466e0ba560983753e23e4dc38
170042 <170042@E0412-05.JC-21.local>
2020-06-15 13:29:40 +0900

    kadai12

--- src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp11;
+package jp.te4a.spring.boot.myapp12;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
@@ -13,7 +13,9 @@ public class BookForm{
 	private Integer id ;
 	@NotNull
 	@Size(min = 3)
+	@TestValid(param = "abc")
 	private String title;
+	@Writter(ok = "東北タロウ")
 	@Size(min = 3, max = 20)
 	private String writter;
 	private String publisher;
---
+++ src/main/java/jp/te4a/spring/boot/myapp12/WritterValidator.java
@@ -0,0 +1,19 @@
+package jp.te4a.spring.boot.myapp12;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class WritterValidator implements ConstraintValidator<Writter,String>{
+	String ok;
+	@Override
+	public void initialize(Writter nv){ ok =  nv.ok(); }
+	@Override
+	public boolean isValid(String in,ConstraintValidatorContext cxt){
+		if(in == null){
+			return false;
+		}
+		System.out.println(in.equals(ok));
+		return in.equals(ok);
+	}
+}
+
